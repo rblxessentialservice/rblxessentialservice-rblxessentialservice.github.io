5d54a4351bc19df094b76753f293831a0abe3e82
DHCGamer1055 <rblxdhcweb@protonmail.com>
2021-06-06 23:10:45 +0100

    a

--- js/function.js
+++ js/function.js
@@ -1,8 +1,25 @@
 const path1 = 'https://rblxessentialservice.github.io/functions?';
 var href = window.location.href
 
+function Scroll(toplevel) {
+    window.scrollTo({top: toplevel, left: 0, behavior: 'smooth'});
+}
+
 if (href == path1 + "create") {
-    var thing = document.getElementById("create")
-    console.log(thing.getBoundingClientRect().top)
-    window.scrollTo({top: thing.getBoundingClientRect().top, left: 0, behavior: 'smooth'});
+    var thing = document.getElementById("create"); Scroll(thing.getBoundingClientRect().top)
+}
+else if (href == path1 + "replace") {
+    var thing = document.getElementById("replace"); Scroll(thing.getBoundingClientRect().top)
+}
+else if (href == path1 + "opacity") {
+    var thing = document.getElementById("opacity"); Scroll(thing.getBoundingClientRect().top)
+}
+else if (href == path1 + "import") {
+    var thing = document.getElementById("import"); Scroll(thing.getBoundingClientRect().top)
+}
+else if (href == path1 + "find") {
+    var thing = document.getElementById("find"); Scroll(thing.getBoundingClientRect().top)
+}
+else if (href == path1 + "findall") {
+    var thing = document.getElementById("findall"); Scroll(thing.getBoundingClientRect().top)
 }
