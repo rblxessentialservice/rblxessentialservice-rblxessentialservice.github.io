7b94d760d74734d3d04053e1e23761c6a04cd2ec
DHCGamer1055 <rblxdhcweb@protonmail.com>
2021-06-06 22:49:52 +0100

    a

--- js/function.js
+++ js/function.js
@@ -4,7 +4,4 @@ var href = window.location.href
 if (href == path1 + "create") {
     var thing = document.getElementById("create")
     window.scrollTo({top: thing.getBoundingClientRect().top , left: 0, behavior: 'smooth'});
-} else if (href == path1 + "replace") { 
-    var thing = document.getElementById("replace")
-    window.scrollTo({top: thing.getBoundingClientRect().top, left: 0, behavior: 'smooth'});
 }
