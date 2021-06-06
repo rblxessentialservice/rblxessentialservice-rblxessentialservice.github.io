b55f2bed9b864ca60ffb901887ec800d0575efa5
DHCGamer1055 <rblxdhcweb@protonmail.com>
2021-06-06 23:02:34 +0100

    Update function.js

--- js/function.js
+++ js/function.js
@@ -3,6 +3,6 @@ var href = window.location.href
 
 if (href == path1 + "create") {
     var thing = document.getElementById("create")
-    console.log(thing.style.top)
-    window.scrollTo({top: thing.style.top , left: 0, behavior: 'smooth'});
+    console.log(thing.getBoundingClientRect().top)
+    window.scrollTo({top: thing.getBoundingClientRect().top, left: 0, behavior: 'smooth'});
 }
