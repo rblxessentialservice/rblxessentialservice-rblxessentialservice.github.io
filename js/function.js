d546d004d70ebd235baf69af046b843a76264a4f
DHCGamer1055 <rblxdhcweb@protonmail.com>
2021-06-06 22:47:39 +0100

    a

--- js/function.js
+++ js/function.js
@@ -1,5 +1,10 @@
 const path1 = 'https://rblxessentialservice.github.io/functions?';
 var href = window.location.href
+
 if (href == path1 + "create") {
-    console.log('You are on page create')
+    var thing = document.getElementById("create")
+    window.scrollTo({top: thing.getBoundingClientRect().top , left: 0, behavior: 'smooth'});
+} else if (href == path1 + "replace") { 
+    var thing = document.getElementById("replace")
+    window.scrollTo({top: thing.getBoundingClientRect().top, left: 0, behavior: 'smooth'});
 }
