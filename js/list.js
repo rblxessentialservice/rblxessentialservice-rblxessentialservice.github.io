fe045ba74fd4886c6b64b805f583ab129000dd3c
DHCGamer1055 <rblxdhcweb@protonmail.com>
2021-06-07 16:33:58 +0100

    gdf

--- js/list.js
+++ js/list.js
@@ -3,6 +3,10 @@ const listItem = document.getElementById('list-item')
 var open = true;
 function OnClickList() {
     if (open == true) {
-        
+        open = false
+        listItem.style.gridTemplateColumns = 'auto 0'
+    } else {
+        open = true
+        listItem.style.gridTemplateColumns = '0 auto'
     }
 }
