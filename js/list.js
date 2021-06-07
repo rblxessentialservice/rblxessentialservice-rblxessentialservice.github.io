cfe53714cf07b7a3c1efc7af3370bbad0c7b3a7b
DHCGamer1055 <rblxdhcweb@protonmail.com>
2021-06-07 16:29:35 +0100

    fgd

---
+++ js/list.js
@@ -0,0 +1,8 @@
+const listButton = document.getElementById('list')
+const listItem = document.getElementById('list-item')
+var open = true;
+function OnClickList() {
+    if (open == true) {
+        
+    }
+}
